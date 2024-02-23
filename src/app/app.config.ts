65755e7a37c6d52e05c73c0a66b8a3319ce184a8
Willy Arrojas <willyarrojas@gmail.com>
2024-02-23 17:56:58 +0100

    header first version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRouting } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()],
+  providers: [provideRouter(AppRouting), provideAnimations()],
 };
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  prefix: '',
+  mode: 'jit',
+  important: false,
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    screens: {
+      xs: '475px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+  },
+  plugins: [],
+};
